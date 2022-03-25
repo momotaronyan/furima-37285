da9bfe9b5b96694568389a8068b1a253906121b6
momotaronyan <momotaronyann@gmail.com>
2022-03-25 10:37:14 +0900

    メインモデル・Active Hashモデルのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  
 end
