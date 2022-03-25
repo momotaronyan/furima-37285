a2980b5f30313af12584e3f961932dc27d7b00e3
momotaronyan <momotaronyann@gmail.com>
2022-03-25 11:46:33 +0900

    選択項目選択可能、form_withの追記、ストロングパラメーターなど

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name,:information, :category_id, :status_id, :burden_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: curret_user.id)
   end
 end
