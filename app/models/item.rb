470984da1929550c4d11d369cf4b1317862ecffa
momotaronyan <momotaronyann@gmail.com>
2022-03-25 10:44:40 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,14 @@ class Item < ApplicationRecord
   belongs_to :burden
   belongs_to :prefecture
   belongs_to :scheduled_delivery
-  
+
+  validates :name, presence: true
+  validates :information, presence: true
+  validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :status_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :burden_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "を選択してください"} 
 end
