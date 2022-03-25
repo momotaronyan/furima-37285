30e8d449a564f9585ace4e6e12cdcbf4bcad00d9
momotaronyan <momotaronyann@gmail.com>
2022-03-25 10:30:46 +0900

    Itemマイグレーション内記述

--- db/migrate/20220325005411_create_items.rb
+++ db/migrate/20220325005411_create_items.rb
@@ -3,7 +3,13 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :name, null: false
       t.text :information, null: false
-      t.integer :
+      t.integer :category_id, null: false
+      t.integer :status_id, null: false
+      t.integer :burden_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :scheduled_delivery_id, null: false
+      t.integer :price, null: false
+      t.references :user, null: false,foreign_key: true
       t.timestamps
     end
   end
