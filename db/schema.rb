21d0e9e0b0650524202ef8853c2c811450f9ecb2
momotaronyan <momotaronyann@gmail.com>
2022-03-26 16:37:22 +0900

    修正3

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,12 @@ ActiveRecord::Schema.define(version: 2022_03_25_020334) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
+    t.string "family_name"
+    t.string "first_name"
+    t.string "family_name_kana"
+    t.string "first_name_kana"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
