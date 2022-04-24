1464be540dc4494183102030985b58cde7d6e0a7
momotaronyan <momotaronyann@gmail.com>
2022-04-24 14:37:17 +0900

    card/user モデルのアソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :items, dependent: :destroy
   has_many :orders, dependent: :destroy
+  has_one :card, dependent: :destroy
 
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true, message: 'には全角文字を使用してください' } do
