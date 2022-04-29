75a42d02d17a865dcbb3ed17b415cd8d68530190
momotaronyan <momotaronyann@gmail.com>
2022-04-29 09:46:08 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   get 'users/show'
   devise_for :users
   root to: 'items#index'
