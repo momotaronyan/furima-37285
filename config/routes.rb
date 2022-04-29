5138343ee69e948100b381f0786d3331b33d941c
momotaronyan <momotaronyann@gmail.com>
2022-04-29 09:54:07 +0900

    コントローラーの編集

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   get 'users/show'
-  devise_for :users
   root to: 'items#index'
   resources :items do
     resources :orders
