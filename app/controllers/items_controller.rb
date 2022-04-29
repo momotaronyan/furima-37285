580a99e2b27e06929857322f6b2764f86f380382
momotaronyan <momotaronyann@gmail.com>
2022-04-29 17:06:46 +0900

    Active Hashと検索画面の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :id_get, only: [:show, :edit, :update, :destroy]
   before_action :return_to_index, only: [:edit, :destroy]
   def index
