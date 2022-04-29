96251979885d3a73c9a3d91f7823a59556a759b6
momotaronyan <momotaronyann@gmail.com>
2022-04-29 09:25:54 +0900

    マイページの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,13 @@ class OrdersController < ApplicationController
   before_action :id_get, only: [:index, :create]
   before_action :return_to_root, only: [:index, :create]
   def index
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
+    card = Card.find_by(user_id: current_user.id) # ユーザーのid情報を元に、カード情報を取得
+    #redirect_to new_card_path and return unless card.present?
+    if card.present?
+      customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
+      @card = customer.cards.first
+    end
     @order_shipping = OrderShipping.new
   end
 
@@ -20,7 +27,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping).permit(:post_code, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:order_shipping).permit(:post_code, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
   def id_get
@@ -29,9 +36,11 @@ class OrdersController < ApplicationController
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer_token = current_user.card.customer_token
     Payjp::Charge.create(
       amount: Item.find(params[:item_id]).price,
-      card: order_params[:token],
+      customer: customer_token,
+      #card: order_params[:token],
       currency: 'jpy'
     )
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
     resources :orders
   end
   resources :cards, only: [:new, :create]
-  resources :users, only: [:show, :update]
+  resources :users, only: [:show, :edit, :update]
 end
