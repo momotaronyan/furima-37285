82e3e762316de8b52034873b094b38090885ee79
momotaronyan <momotaronyann@gmail.com>
2022-05-10 12:11:42 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Furima37285
     # 日本語設定
     config.i18n.default_locale = :ja
 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
